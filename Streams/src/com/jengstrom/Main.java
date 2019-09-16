3b2c869fb54e62d6ca331c84f4d7a7e245a681bd
Jordan Engstrom <jordan.engstrom@gmail.com>
2019-09-15 21:10:40 -0600

    using streams

---
+++ Streams/src/com/jengstrom/Main.java
@@ -0,0 +1,111 @@
+package com.jengstrom;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> someBingoNumbers = Arrays.asList(
+                "N40", "N36",
+                "B12", "B6",
+                "G53", "G49", "G60", "G50", "g64",
+                "I26", "I17", "I29",
+                "O71");
+
+        List<String> gNumbers = new ArrayList<>();
+
+//      WITHOUT STREAMS
+//        someBingoNumbers.forEach(number-> {
+//            if(number.toUpperCase().startsWith("G")){
+//                gNumbers.add(number);
+//            }
+//        });
+//
+//        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
+//        gNumbers.forEach((String s) -> System.out.println(s));
+//
+
+//      USING STREAMS:
+//      A stream is a set of object references.
+        someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s->s.startsWith("G"))
+                .sorted()
+                .forEach(System.out::println);
+                // .forEach here is called a terminal operation
+                // a terminal operation returns a void or non-stream result
+                // the stream ends on terminal operations
+                // intermediate operations return stream results
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I17", "I29", "O71");
+        Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
+        System.out.println("===================================================");
+        System.out.println(concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count());
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jack = new Employee("Jack Doe", 25);
+        Employee jane = new Employee("Jane Doe", 40);
+        Employee jordan = new Employee("Jordan Doe", 22);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(john);
+        hr.addEmployee(jack);
+        hr.addEmployee(jane);
+        Department accounting = new Department("Accounting");
+        hr.addEmployee(jordan);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+
+        System.out.println("===================================================");
+//        List<String> sortedGNumbers = someBingoNumbers
+//                .stream()
+//                .map(String::toUpperCase)
+//                .filter(s-> s.startsWith("G"))
+//                .sorted()
+//                .collect(Collectors.toList());
+
+        List<String> sortedGNumbers = someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s-> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+
+        for(String s : sortedGNumbers) {
+            System.out.println(s);
+        }
+        Map<Integer, List<Employee>> groupedByAge = departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .collect(Collectors.groupingBy(Employee::getAge));
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2)-> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+        Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST")
+                .filter(s -> {
+                    System.out.println(s);
+                    return  s.length() == 3;
+                })
+                .count();
+    }
+}
