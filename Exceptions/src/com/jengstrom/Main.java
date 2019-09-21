6ffceaf9e62c5e430371b60de1ee140a4340f3be
Jordan Engstrom <jordan.engstrom@gmail.com>
2019-09-20 21:46:56 -0600

    handling multiple exceptions

--- Exceptions/src/com/jengstrom/Main.java
+++ Exceptions/src/com/jengstrom/Main.java
@@ -18,56 +18,60 @@ public class Main {
 //        System.out.println(divideEAFP(x, y));
 //        System.out.println(divide(x, y));
 
+//        int x = getInt();
+
 //        int x = getIntLBYL();
         int x = getIntEAFP();
         System.out.println("x is " + x);
-    }
 
-    private static int getIntEAFP() {
-        System.out.println("Pick an integer: ");
-        Scanner scanner = new Scanner(System.in);
-        try {
-            return scanner.nextInt();
-        } catch(InputMismatchException e) {
+
+
+    }
+        private static int getInt() {
+            Scanner s = new Scanner(System.in);
+            return s.nextInt();
+        }
+
+        private static int getIntLBYL() {
+            Scanner s = new Scanner(System.in);
+            boolean isValid = true;
+            System.out.println("Please enter an integer ");
+            String input = s.next();
+            for (int i=0; i<input.length(); i++) {
+                if(!Character.isDigit(input.charAt(i))) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if(isValid) {
+                return Integer.parseInt(input);
+            }
             return 0;
         }
-    }
 
-    private static int getInt() {
-        System.out.println("Pick an integer: ");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-
-    private static int getIntLBYL() {
-        System.out.println("Pick an integer: ");
-        Scanner scanner = new Scanner(System.in);
-        Boolean isValid = true;
-        String input = scanner.next();
-        for (int i=0; i<input.length(); i++) {
-            if(!Character.isDigit(input.charAt(i))){
-                isValid = false;
-                break;
+        private static int getIntEAFP() {
+        Scanner s = new Scanner(System.in);
+            System.out.println("Please enter an integer");
+            try {
+                return s.nextInt();
+            } catch(InputMismatchException e) {
+                return 0;
             }
         }
-        if (isValid) {
-            return Integer.parseInt(input);
-        }
-        return 0;
-    }
 
-    private static int divideLBYL(int x, int y) {
-        if (y != 0) {
-            return x / y;
-        } else {
-            return 0;
+        private static int divideLBYL(int x, int y) {
+            if (y != 0){
+                return x / y;
+            } else {
+                return 0;
+            }
         }
-    }
 
-    private static int divideEAFP(int x, int y){
+        private static int divideEAFP(int x, int y) {
         try {
             return x / y;
-        } catch (ArithmeticException e) {
+
+        } catch(ArithmeticException e) {
             return 0;
         }
     }
